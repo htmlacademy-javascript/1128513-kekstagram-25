96fbfd60d41240c643e300cc55783122322c65e3
alexandrashmoylova <alexandra_shmoylova@yahoo.com>
2022-02-21 15:41:03 +0400

    Исправляет ошибки

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 const getRandomIntFromRange = function (min, max) {
   if (max <= min) {
-    throw new Error("Значение min должно быть больше значения max!");
+    throw new Error('Значение min должно быть больше значения max!');
   }
 
   if (min < 0 || max < 0) {
-    throw new Error("Диапазон должен быть положительным!");
+    throw new Error('Диапазон должен быть положительным!');
   }
   min = Math.ceil(min);
   max = Math.floor(max);
