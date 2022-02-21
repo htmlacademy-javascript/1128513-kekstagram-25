24c110cae662ef03b6983255637bd84fa113871e
alexandrashmoylova <alexandra_shmoylova@yahoo.com>
2022-02-21 15:38:15 +0400

    Исправляет ошибки

--- js/main.js
+++ js/main.js
@@ -1,24 +1,24 @@
 const getRandomIntFromRange = function (min, max) {
-    if (max <= min) {
-      throw new Error('Значение min должно быть больше значения max!');
-    }
-  
-    if (min < 0 || max < 0) {
-      throw new Error('Диапазон должен быть положительным!');
-    }
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  };
+  if (max <= min) {
+    throw new Error("Значение min должно быть больше значения max!");
+  }
 
-getRandomIntFromRange (4, 10);
-  
+  if (min < 0 || max < 0) {
+    throw new Error("Диапазон должен быть положительным!");
+  }
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
 
-  const getStringLength = function (currentString, stringLength) {
-      const maxLength = 140;
-    if (stringLength <= maxLength) {
-        return true;
-    } return false;
-  };
+getRandomIntFromRange(4, 10);
 
-getStringLength (2, 120);
+const getStringLength = function (currentString, stringLength) {
+  const maxLength = 140;
+  if (stringLength <= maxLength) {
+    return true;
+  }
+  return false;
+};
+
+getStringLength(2, 120);
