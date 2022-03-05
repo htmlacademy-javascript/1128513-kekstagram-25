d5c8b940c4c0f1608b6224c7f44fcd1dabfe975b
Keks <keks@htmlacademy.ru>
2022-03-05 10:28:57 +0300

    :heavy_check_mark: Сборка #3

--- 3/js/main.js
+++ 3/js/main.js
@@ -1,5 +1,5 @@
 const getRandomIntFromRange = function (min, max) {
-  if (max <= min) {
+  if (max < min) {
     throw new Error('Значение min должно быть больше значения max!');
   }
 
@@ -10,8 +10,7 @@ const getRandomIntFromRange = function (min, max) {
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
-
-getRandomIntFromRange(4, 10);
+getRandomIntFromRange(1, 2);
 
 const checkStringLength = function (currentString, maxLength) {
   return currentString.length <= maxLength;
@@ -19,55 +18,56 @@ const checkStringLength = function (currentString, maxLength) {
 
 checkStringLength('Длина строки', 10);
 
-
-const DESCRIPTIONS = ['Good morning world', 'Это я был на Эльбрусе', 'Жизнь прекрасна!', 'Выставка арт объектов', 'Завтрак у моря', 'Когда встал в 6 утра'];
-const COMMENTS = ['Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'В целом всё неплохо. Но не всё.'];
-const NAMES = ['Пётр Васильев','Диана Рублёва', 'Александр Тимошкин', 'Илья Гроденберг'];
-const DESCRIPTION_QUANTITY = 25;
-const COMMENTS_QUANTITY = 6;
-const ID_LIST = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
-const getRandomArrayElement = (elements) => elements[getRandomIntFromRange(0, elements.length - 1)];
-
 // Функция, создающая не повторяющийся массив элементов
 const getRandomUniqueElements = (arr) => {
   // создаю копию передавемого массива
   const newArray = arr.slice();
   // создаю новый пустой массив, куда буду складывать новые не повторяющиеся элементы
   const elements = [];
+  const newArrayLength = arr.length;
   // Запускаю цикл,  который будет идти столько раз сколько и элементов в массиве
-  for (let i = 0; i < newArray.length - 1; i++) {
-  // создаю переменную, которая генерирут случ элемент в переданном массиве
-    const id = getRandomIntFromRange(0, newArray.length - 1);
-    // добавляю в новый массив рандомный элемент из массива newArray
-    elements.push(newArray[id]);
+  for (let i = 0; i < newArrayLength; i++) {
+  // создаю переменную, которая генерирут случ значение в переданном массиве
+    const randomId = getRandomIntFromRange(0, newArray.length- 1);
+    // добавляю в новый массив рандомное значение из массива newArray
+    elements.push(newArray[randomId]);
     //  удаляю из newArray этот индекс элемента
-    newArray.splice(id, 1);
+    newArray.splice(randomId, 1);
   }
   return elements;
 };
 
-getRandomUniqueElements(ID_LIST);
+const getRandomArrayElement = (elements) => elements[getRandomIntFromRange(0, elements.length - 1)];
 
-const createComment = () => ({
-  id: 1,
+const DESCRIPTIONS = ['Good morning world', 'Это я был на Эльбрусе', 'Жизнь прекрасна!', 'Выставка арт объектов', 'Завтрак у моря', 'Когда встал в 6 утра'];
+const COMMENTS = ['Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'В целом всё неплохо. Но не всё.'];
+const NAMES = ['Пётр Васильев','Диана Рублёва', 'Александр Тимошкин', 'Илья Гроденберг'];
+const DESCRIPTION_QUANTITY = 25;
+const COMMENTS_QUANTITY = 6;
+const ID_LIST = getRandomUniqueElements(Array.from({ length: 25 }, (v, k) => ++k));
+// ++k сначала получает элемент, затем увеличивает
+
+const createComment = (photoId, commentId) => ({
+  // ID фотографии * макс. кол-во комментариев + ID коммента
+  id: photoId * COMMENTS_QUANTITY + commentId,
   avatar: `img/avatar-${getRandomIntFromRange(1,6)}.svg`,
   message: getRandomArrayElement(COMMENTS),
   name: getRandomArrayElement(NAMES),
 });
 
-function createPhotoDescription () {
+const createPhotoDescription = () => {
+  const id = ID_LIST.shift();
   const description = {
-    id: 1,
-    url: `photos/${getRandomIntFromRange(1, 25)}.jpg`,
+    id: id,
+    url: `photos/${id}.jpg`,
     description: getRandomArrayElement(DESCRIPTIONS),
     likes: getRandomIntFromRange(15, 200),
-    comments: Array.from({length: COMMENTS_QUANTITY}, createComment),
+    comments: Array.from({length: COMMENTS_QUANTITY}, (v, k) => createComment(id, k)),
   };
 
   return description;
-}
+};
 
-const allDescriptions = Array.from({length: DESCRIPTION_QUANTITY}, createPhotoDescription);
+const allDescriptions = () => Array.from({length: DESCRIPTION_QUANTITY}, createPhotoDescription);
 
 allDescriptions();
