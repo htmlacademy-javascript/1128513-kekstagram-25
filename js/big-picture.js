9e219d325b91c94651e7267fc78b6cdc0ecac661
alexandrashmoylova <alexandra_shmoylova@yahoo.com>
2022-04-14 12:47:25 +0400

    комментарии(исправления)

--- js/big-picture.js
+++ js/big-picture.js
@@ -8,31 +8,9 @@ const commentsContainer = bigPicture.querySelector('.social__comments');
 const commentsToShowCount = bigPicture.querySelector('.social__comment-count');
 const body = document.querySelector('body');
 let count = 0;
-let comments = [];
-
-const onPopupEscKeydown = (evt) => {
-  if (isEscapeKey(evt)) {
-    evt.preventDefault();
-    closeBigPicture ();
-  }
-};
-
-const onPopupCloseButtonClick = () => {
-  closeBigPicture();
-};
-
-function closeBigPicture () {
-  bigPicture.classList.add('hidden');
-  body.classList.remove('modal-open');
-  document.removeEventListener('keydown', onPopupEscKeydown);
-  document.removeEventListener('click', onPopupCloseButtonClick);
-  commentsLoader.removeEventListener('click', commentsLoaderOnClick);
-  count = 0;
-  comments = [];
-}
 
 const createCommentItem = (comment) => {
-  const newCommentItem  = document.createElement('li');
+  const newCommentItem = document.createElement('li');
   newCommentItem.classList.add('social__comment');
   const commentImage = document.createElement('img');
   commentImage.classList.add('social__picture');
@@ -49,33 +27,54 @@ const createCommentItem = (comment) => {
   return newCommentItem;
 };
 
-function commentsLoaderOnClick() {
-  // изменяем значение сount прибавляя 5, следовательно slice станет (5, 10), отрисуется еще 5 штук
-  count += MAX_COMMENTS_TO_SHOW;
-  renderCommentsSlice();
-}
-
-function renderCommentsSlice() {
-  commentsContainer.innerHTML = '';
-  const commentsFragment = document.createDocumentFragment();
-  // создаем срез комментов, будет показываться 5 штук, при клике count перезапишется
-  const commentsToShow = comments.slice(0, count + MAX_COMMENTS_TO_SHOW);
-  commentsToShow.forEach((comment) => {
-    commentsFragment.appendChild(createCommentItem(comment));
-  });
-  commentsContainer.appendChild(commentsFragment);
-  commentsLoader.classList.toggle('hidden', comments.length === commentsToShow.length);
-  commentsToShowCount.innerHTML = `${commentsToShow.length} из <span class="comments-count">${comments.length}</span> комментариев`;
-}
-
 const showBigPicture = (picture) => {
+
+  const onPopupEscKeydown = (evt) => {
+    if (isEscapeKey(evt)) {
+      evt.preventDefault();
+      closeBigPicture();
+    }
+  };
+
+  const onPopupCloseButtonClick = () => {
+    closeBigPicture();
+  };
+
+  function closeBigPicture() {
+    bigPicture.classList.add('hidden');
+    body.classList.remove('modal-open');
+    document.removeEventListener('keydown', onPopupEscKeydown);
+    document.removeEventListener('click', onPopupCloseButtonClick);
+    commentsLoader.removeEventListener('click', commentsLoaderOnClick);
+    count = 0;
+  }
+
+  function commentsLoaderOnClick() {
+    // изменяем значение count прибавляя 5, следовательно slice станет (5, 10), отрисуется еще 5 штук
+    count += MAX_COMMENTS_TO_SHOW;
+    renderCommentsSlice();
+  }
+
+  function renderCommentsSlice() {
+    commentsContainer.innerHTML = '';
+    const commentsFragment = document.createDocumentFragment();
+    // создаем срез комментов, будет показываться 5 штук, при клике count перезапишется
+    const commentsToShow = picture.comments.slice(0, count + MAX_COMMENTS_TO_SHOW);
+    commentsToShow.forEach((comment) => {
+      commentsFragment.appendChild(createCommentItem(comment));
+    });
+    commentsContainer.appendChild(commentsFragment);
+    commentsLoader.classList.toggle('hidden', picture.comments.length === commentsToShow.length);
+    commentsToShowCount.innerHTML = `${commentsToShow.length} из <span class="comments-count">${picture.comments.length}</span> комментариев`;
+  }
+
   bigPicture.classList.remove('hidden');
   body.classList.add('modal-open');
   bigPicture.querySelector('.big-picture__img img').src = picture.url;
   bigPicture.querySelector('.likes-count').textContent = picture.likes;
   bigPicture.querySelector('.comments-count').textContent = picture.comments.length;
   bigPicture.querySelector('.social__caption').textContent = picture.description;
-  comments = picture.comments;
+
   renderCommentsSlice();
   commentsLoader.addEventListener('click', commentsLoaderOnClick);
 
